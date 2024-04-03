6839aaff68f8aa31c1601e3fe36599c083135d0a
pulkit-gautam <gautampulkit@iitgn.ac.in>
2024-04-03 18:51:15 +0530

    Some Updates

--- server/config.py
+++ server/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost/maintainence'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Pulkit#69@localhost/maintainence'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'inwowmew'
