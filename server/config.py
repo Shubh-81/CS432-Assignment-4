beadb168a08cf540b1cb654047bcd1cd9ecd3a88
Atal Gupta <aatall1122@gmail.com>
2024-04-16 16:12:05 +0530

    Malacious.html

--- server/config.py
+++ server/config.py
@@ -1,8 +1,9 @@
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost/maintainence'
 import urllib.parse
-encoded_password = urllib.parse.quote_plus('Am123456@')
+encoded_password = urllib.parse.quote_plus('Atal@2207')
 
 # Construct the updated SQLAlchemy database URI
 SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:{encoded_password}@localhost/maintenance'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'inwowmew'
+WTF_CSRF_ENABLED = True
